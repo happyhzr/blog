c019e4b433341793ab4143610f89519961d4d6dd
happyhzr <insisthzr@163.com>
2026-07-09 21:02:20 +0800

    add crud

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,13 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./blog.db"
+POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg://postgres:{POSTGRES_PASSWORD}@db:5432/postgres"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False},
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
