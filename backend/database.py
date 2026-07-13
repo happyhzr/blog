357a2fc513524afebce18953530f2e79f872605a
happyhzr <insisthzr@163.com>
2026-07-13 20:55:30 +0800

    add async

--- backend/database.py
+++ backend/database.py
@@ -2,21 +2,22 @@ import os
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
-SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg://postgres:{POSTGRES_PASSWORD}@db:5432/postgres"
+SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://postgres:{POSTGRES_PASSWORD}@db:5432/postgres"
 
-engine = create_engine(
+engine = create_async_engine(
     SQLALCHEMY_DATABASE_URL,
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+AsyncSessionLocal = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
 class Base(DeclarativeBase):
     pass
 
 
-def get_db():
-    with SessionLocal() as db:
-        yield db
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
